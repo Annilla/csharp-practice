dd48a3df0d5d44a3ae3ed958356292871dba9d0e
Annilla <anny09117011@gmail.com>
2020-07-28 15:46:54 +0800

    [Sec2]

---
+++ practice2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 16. If-Else Statements
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            if(age >= 18)
+            {
+                Console.WriteLine("You're good to go.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you aren't 18 yet.");
+            }
+
+            // 18. Switch Statements
+            Console.WriteLine("Enter your year");
+            int year = int.Parse(Console.ReadLine());
+            switch (year)
+            {
+                case 1:
+                {
+                    Console.WriteLine("Freshman");
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("Sophomore");
+                    break;
+                }
+                case 3:
+                {
+                    Console.WriteLine("Junior");
+                    break;
+                }
+                case 4:
+                {
+                    Console.WriteLine("Senior");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Invalid year");
+                    break;
+                }
+            }
+
+        }
+    }
+}
