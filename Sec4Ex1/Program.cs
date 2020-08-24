1e7a0284425de9f7694d6d1dccea0ae8c0a29709
Annilla <anny09117011@gmail.com>
2020-08-24 18:15:33 +0800

    [Sec4] Ex1

---
+++ Sec4Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sec4Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zipArray = new int[10]{ 111, 222, 333, 444, 555, 666, 777, 888, 999, 000 };
+
+            string[] compArray = new string[10]{ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+            Console.WriteLine("Enter zip code");
+            int code = int.Parse(Console.ReadLine());
+            bool found = false;
+
+            for(int i = 0; i < zipArray.Length; i++) {
+                if(code == zipArray[i]) {
+                    Console.WriteLine("Zip {0} is company {1}.", code, compArray[i]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found) {
+                Console.WriteLine("Zip {0} is not found.", code);
+            }
+        }
+    }
+}
---
+++ Sec4Ex2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Sec4Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList student = new ArrayList();
+            ArrayList score = new ArrayList();
+            ArrayList sortScore = new ArrayList();
+            string keyName;
+            int keyScore;
+            int total = 0;
+            int average = 0;
+
+            do{
+                Console.WriteLine("Enter student's name or press 'Q' to exit.");
+                keyName = Console.ReadLine();
+
+                if(keyName != "Q") {
+                    student.Add(keyName);
+                    Console.WriteLine("Enter {0}'s score.", keyName);
+                    keyScore = int.Parse(Console.ReadLine());
+                    score.Add(keyScore);
+                    sortScore.Add(keyScore);
+                }
+                
+
+            } while (keyName != "Q");
+
+            sortScore.Sort(); 
+
+            for(int i = 0; i < score.Count ; i++) {
+                int num = int.Parse(score[i]);
+                total = total + num;
+                if(sortScore[0] == score[i]) {
+                    Console.WriteLine("Highest grade is {0} {1}.", student[i], sortScore[0]);
+                }
+            }
+
+            average = total / (score.Count);
+
+            Console.WriteLine("Average grade is {0}.", average);
+            
+        }
+    }
+}
